06f2be8d77434fe03a52a9d78813bef9a9734ed2
AlbinSverreson <albin.sverreson@gmail.com>
2021-10-16 15:37:44 +0200

    lab 5 nästan fungerande sekvensiellt program

--- labs/lab5/preflow/src/main.rs
+++ labs/lab5/preflow/src/main.rs
@@ -32,6 +32,53 @@ impl Edge {
         }
 }
 
+fn enter_excess(excess: &mut VecDeque<usize>, node: &usize, n: &usize){
+  //println!("1");
+  if (*node != 0) && (*node != n-1) {
+  //println!("2");
+    excess.push_back(*node);
+  }
+  //println!("3");
+}
+
+fn leave_excess(excess: &mut VecDeque<usize>) -> usize{
+  return excess.pop_front().unwrap();
+}
+
+fn other(u: &usize, edge: &Edge) -> usize{
+  if edge.u == *u { return edge.v }
+  else { return *u }
+}
+
+fn relabel(excess: &mut VecDeque<usize>, u: &mut Node, n: &usize){
+    println!("Relabling {}", u.i);
+    u.h += 1;
+    enter_excess(excess, &u.i, &n);
+}
+
+fn push(excess: &mut VecDeque<usize>, u: &mut Node, v: &mut Node, e: &mut Edge, n: &usize){
+  let d: i32;
+  if u.i == e.u{
+    d = cmp::min(u.e, e.c - e.f);
+    e.f += d;
+  } else {
+    d = cmp::min(u.e, e.c + e.f);
+    e.f -= d;
+  }
+	println!("pushing {} from {} to {}", d, u.i, v.i);
+
+  u.e -= d;
+  v.e += d;
+
+  if u.e > 0 {
+    enter_excess(excess, &u.i, &n);
+  }
+
+  if v.e == d {
+    enter_excess(excess, &v.i, &n);
+  }
+}
+
 
 fn main() {
 
@@ -81,14 +128,49 @@ fn main() {
 
 	println!("initial pushes");
 	let iter = adj[s].iter();
+  for e in iter {
+    let v = other(&s, &edge[*e].lock().unwrap());
+    node[s].lock().unwrap().e += edge[*e].lock().unwrap().c;
+    println!("haj du {}", node[s].lock().unwrap().e);
 
+    push(&mut excess, &mut node[s].lock().unwrap(), &mut node[v].lock().unwrap(), &mut edge[*e].lock().unwrap(), &n); 
+  }
 	// but nothing is done here yet...
 
 	while !excess.is_empty() {
 		let mut c = 0;
-		let u = excess.pop_front().unwrap();
+    let mut b = 1;
+    let mut v = n;
+    let mut e_index = 0; 
+		let u = leave_excess(&mut excess);
+    //println!("ayy lmao");
+	  let iter = adj[u].iter();
+    for e in iter {
+      e_index = *e;
+      v = other(&u, &edge[*e].lock().unwrap());
+      if u != *e {
+        b = -1;
+      }
+      
+      let u_h = node[u].lock().unwrap().h;    
+      let v_h = node[v].lock().unwrap().h;
+      let e_f = edge[*e].lock().unwrap().f;
+      let e_c = edge[*e].lock().unwrap().c; 
+      if ( u_h > v_h)  && (b*e_f<e_c) {
+        break;
+      }
+      else  {
+        v = n;
+      }
+    }
+    if v != n {
+      push(&mut excess, &mut node[u].lock().unwrap(), &mut node[v].lock().unwrap(), &mut edge[e_index].lock().unwrap(), &n); 
+    } else {
+      relabel(&mut excess, &mut node[u].lock().unwrap(), &n);
+    }
+  
 	}
 
-	println!("f = {}", 0);
+	println!("f = {}", node[t].lock().unwrap().e);
 
 }
