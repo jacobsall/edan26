2313f55d326cf1463a5b37e6a0e714f8e671317f
AlbinSverreson <albin.sverreson@gmail.com>
2021-10-18 15:39:26 +0200

    tog bort skitfilerna mvh tbh

--- labs/lab5/preflow_multi/target/debug/deps/preflow-ca241affcdb8984f.d
+++
@@ -1,5 +0,0 @@
-/home/jacob/Documents/edan26/labs/lab5/preflow/target/debug/deps/preflow-ca241affcdb8984f: src/main.rs
-
-/home/jacob/Documents/edan26/labs/lab5/preflow/target/debug/deps/preflow-ca241affcdb8984f.d: src/main.rs
-
-src/main.rs:
--- labs/lab5/preflow_multi/target/debug/preflow.d
+++
@@ -1,1 +0,0 @@
-/home/jacob/Documents/edan26/labs/lab5/preflow/target/debug/preflow: /home/jacob/Documents/edan26/labs/lab5/preflow/src/main.rs
